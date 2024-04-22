ca8d00d16006d5c01220e8d98fda76b4adcf6d2c
Haseeb Raza <haseeb.javed715@gmail.com>
2024-04-22 13:02:25 +0200

    reslove the bug

--- src/api_controller.py
+++ src/api_controller.py
@@ -1,6 +1,6 @@
 """Patient API Controller"""
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from patient_db import PatientDB
 
 
@@ -33,19 +33,49 @@ class PatientAPIController:
     """
 
     def create_patient(self):
-        pass
+        request_body = request.json
+        patient_id = self.patient_db.insert_patient(request_body)[0]
+        print(patient_id)
+        if patient_id:
+            response_body = {"patient_id": patient_id}
+            
+            status_code = 200
+        else:
+            response_body = {"error": "Failed to create patient"}
+            status_code = 400
+        return jsonify(response_body), status_code
 
+        
     def get_patients(self):
-        pass
-
+        patients = self.patient_db.select_all_patients()
+        if patients:
+            return jsonify(patients), 200
+        else:
+            return jsonify({"error": "Failed to retrieve patients"}), 400
+        
     def get_patient(self, patient_id):
-        pass
+        patient = self.patient_db.select_patient(patient_id)
+        if patient:
+            return jsonify(patient), 200
+        else:
+            return jsonify({"error": "Patient not found"}), 400
+        
 
     def update_patient(self, patient_id):
-        pass
+        data = request.json
+        response = self.patient_db.update_patient(patient_id, data)
+        if response:
+            return jsonify({"message": "Patient updated successfully"}), 200
+        else:
+            return jsonify({"error": "Failed to update patient"}), 400
+        
 
     def delete_patient(self, patient_id):
-        pass
+        response = self.patient_db.delete_patient(patient_id)
+        if response:
+            return jsonify({"message": "Patient deleted successfully"}), 200
+        else:
+            return jsonify({"error": "Failed to delete patient"}), 400
 
     def run(self):
         """
--- src/patient.py
+++ src/patient.py
@@ -16,3 +16,83 @@ Validation should be done for all of the variables in config and db_config.
 
 There should be a method to commit that patient to the database using the api_controller.
 """
+
+import uuid
+from datetime import datetime
+from api_controller import PatientAPIController
+from config import GENDERS, WARD_NUMBERS, ROOM_NUMBERS, API_CONTROLLER_URL
+from patient_db_config import PATIENT_COLUMN_NAMES
+import requests
+
+class Patient:
+    def __init__(self, name, gender, age):
+        self.patient_id = str(uuid.uuid4())
+        self.patient_name = self.validate_name(name)
+        self.patient_gender = self.validate_gender(gender)
+        self.patient_age = self.validate_age(age)
+        self.patient_checkin = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.patient_checkou = None
+        self.patient_ward = None
+        self.patient_room = None
+
+
+    def validate_name(self, name):
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string")
+        return name
+
+    def validate_gender(self, gender):
+        if gender not in GENDERS:
+            raise ValueError("Invalid gender")
+        return gender
+
+    def validate_age(self, age):
+        try:
+            age = int(age)
+            if age < 0:
+                raise ValueError("Age cannot be negative")
+            return age
+        except ValueError:
+            raise ValueError("Age must be an integer")
+
+    def validate_ward(self, ward):
+        if ward not in WARD_NUMBERS:
+            raise ValueError("Invalid ward number")
+        return ward
+    
+    def set_checkout_info(self, ward, room):
+        self.patient_checkout = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.patient_ward = self.validate_ward(ward)
+        self.patient_room = self.validate_room(room)
+
+    def validate_room(self, ward, room):
+        if room not in ROOM_NUMBERS[ward]:
+            raise ValueError("Invalid room number for the ward")
+        return room
+
+    def update_room_and_ward(self, ward, room):
+        if (ward in WARD_NUMBERS and room in ROOM_NUMBERS[ward]):
+            self.patient_ward = ward
+            self.patient_room = room
+            print("Room and ward updated successfully.")
+        else:
+            print("Invalid ward or room number.")
+
+
+    def commit_to_database(self):
+            data = {
+                
+                "patient_name": self.patient_name,
+                "patient_age": self.patient_age,
+                "patient_gender": self.patient_gender,
+                "patient_ward": self.patient_ward,
+                "patient_room": self.patient_room
+            }
+            response = requests.post(f"{API_CONTROLLER_URL}/patients", json=data)
+            if response.status_code == 200:
+                print("Patient data committed successfully.")
+                response_data = response.json()
+                self.patient_id = response_data.get("patient_id")
+                self.patient_checkin = response_data.get("patient_checkin")
+            else:
+                print("Failed to commit patient data to the database.")
